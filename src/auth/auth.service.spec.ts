6eb85ee228b6edc330466ac01fb941c59b57d89f
crispy <dltmdcks702@cau.ac.kr>
2024-09-08 19:49:32 +0900

    feat: add fail test for interviewer

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,11 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Interviewer } from '../entities/interviewer';
+import { CreateInterviewerDTO } from './dto/createInterviewer.dto';
+import { plainToInstance } from 'class-transformer';
+import { testConfig } from '../mikro-orm.config';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [MikroOrmModule.forRoot(testConfig)],
       providers: [AuthService],
     }).compile();
 
@@ -16,5 +22,12 @@ describe('AuthService', () => {
     expect(service).toBeDefined();
   });
 
-  
+  it('20자 미만, 0자 이상의 입력에 대해서 interviewer를 생성함', async () => {
+    const name = 'validName';
+    const createDTO = plainToInstance(CreateInterviewerDTO, { name });
+
+    const interviewer = await service.createInterviewer(createDTO);
+
+    expect(interviewer).toBeInstanceOf(Interviewer);
+  });
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateInterviewerDTO } from './dto/createInterviewer.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  createInterviewer(createDTO: CreateInterviewerDTO) {
+    throw new Error('Method not implemented.');
+  }
+}
