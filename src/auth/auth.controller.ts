771b8c6330d812faa624baf08fe0d23eeebef214
crispy <dltmdcks702@cau.ac.kr>
2024-09-09 20:03:54 +0900

    feat: add auth controller test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateInterviewerDTO } from './dto/createInterviewer.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  createInterviewer(dto: CreateInterviewerDTO) {
+    return this.authService.createInterviewer(dto);
+  }
 }
