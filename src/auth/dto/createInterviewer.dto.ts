2eb5473fd10d504175957e7fb2644f7c0b940600
crispy <dltmdcks702@cau.ac.kr>
2024-09-09 21:05:35 +0900

    feat: make auth api work with edge case test

--- src/auth/dto/createInterviewer.dto.ts
+++ src/auth/dto/createInterviewer.dto.ts
@@ -1,11 +1,10 @@
 import { Exclude, Expose } from 'class-transformer';
-import { IsString, MaxLength, MinLength } from 'class-validator';
+import { Length, IsString } from 'class-validator';
 
 @Exclude()
 export class CreateInterviewerDTO {
   @Expose()
-  @MaxLength(20)
-  @MinLength(1)
   @IsString()
+  @Length(1, 20)
   name: string;
 }
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,19 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { testConfig } from '../src/mikro-orm.config';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule, MikroOrmModule.forRoot(testConfig)],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+      }),
+    );
+
     await app.init();
   });
 
@@ -24,8 +28,8 @@ describe('AuthController (e2e)', () => {
       return request(app.getHttpServer())
         .post('/login')
         .send({ name: testName })
-        .expect(200)
-        .expect('name', testName);
+        .expect(201)
+        .expect({ name: testName });
     });
 
     it('1자 이름에 대해서 성공한다.', () => {
@@ -34,12 +38,30 @@ describe('AuthController (e2e)', () => {
       return request(app.getHttpServer())
         .post('/login')
         .send({ name: testName })
-        .expect(200)
-        .expect('name', testName);
+        .expect(201)
+        .expect({ name: testName });
     });
 
     it('21자 이름에 대해서 실패한다.', () => {
       const testName = 'a'.repeat(21);
+
+      return request(app.getHttpServer())
+        .post('/login')
+        .send({ name: testName })
+        .expect(400);
+    });
+
+    it('0자 이름에 대해서 실패한다.', () => {
+      const testName = '';
+
+      return request(app.getHttpServer())
+        .post('/login')
+        .send({ name: testName })
+        .expect(400);
+    });
+
+    it('숫자에 대해서 실패한다.', () => {
+      const testName = 11;
 
       return request(app.getHttpServer())
         .post('/login')
