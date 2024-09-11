2ca2ab9b98adf162b8facf4418ef6b3624dc568a
crispy <dltmdcks702@cau.ac.kr>
2024-09-12 03:44:48 +0900

    feat: add api tags on reviewers

--- src/reviewers/reviewers.controller.ts
+++ src/reviewers/reviewers.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { ReviewersService } from './reviewers.service';
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ReviewerDTO } from './dto/reviwer.dto';
 
+@ApiTags('reviewers')
 @Controller('reviewers')
 export class ReviewersController {
   constructor(private readonly reviwersService: ReviewersService) {}
