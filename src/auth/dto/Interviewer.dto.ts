fdb031315fcccb024ca2eccfd519bc583e665124
crispy <dltmdcks702@cau.ac.kr>
2024-09-12 03:35:59 +0900

    feat: add property for response

--- src/auth/dto/Interviewer.dto.ts
+++ src/auth/dto/Interviewer.dto.ts
@@ -1,9 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 import { Interviewer } from 'src/entities/interviewer';
 
 @Exclude()
 export class InterviewerDTO {
   @Expose()
+  @ApiProperty()
   name: string;
 
   static fromEntity(entity: Interviewer): InterviewerDTO {
--- src/auth/dto/createInterviewer.dto.ts
+++ src/auth/dto/createInterviewer.dto.ts
@@ -1,9 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 import { Length, IsString } from 'class-validator';
 
 @Exclude()
 export class CreateInterviewerDTO {
   @Expose()
+  @ApiProperty()
   @IsString()
   @Length(1, 20)
   name: string;
